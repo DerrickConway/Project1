071773d3e3e1b48cda4d85835178ff7cf114cf32
DerrickConway <derrick.conway15@gmail.com>
2016-11-24 21:07:38 +0000

    Database Connected

--- Project1.py
+++ Project1.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask import request, redirect, url_for
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:declanconn1@localhost/flaskInfo'
+app.debug = True
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+        def __repr__(self):
+            return '<User %r>' % self.username
 
 
 @app.route("/")
@@ -9,8 +26,10 @@ def HomePage():
 
 @app.route("/post_user",methods=["POST"])
 def post_user():
-    return render_template("HomePage.html")
-
+    user = User(request.form['username'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('HomePage'))
 
 
 if __name__ == '__main__':
